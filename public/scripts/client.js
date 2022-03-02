5db75320f9c1b0b5a41750e7d131fc65a2a0d194
Sasu Osayande <osayande.sasu@outlook.com>
2022-03-02 18:44:17 -0500

    Implement the loadTweets function and make an ajax GET request. Remove temporary hard-coded tweets object

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,73 +4,58 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
+$(document).ready(function () {
+  const renderTweets = function (tweetData) {
+    // loops through tweets
+    for (let tweet of tweetData) {
+      // calls createTweetElement for each tweet
+      createTweetElement(tweet);
+    }
+  };
+  // renderTweets(data);
 
-const createTweetElement = function(tweet) {
-  let $tweetPost = `
-  <article class="tweet-border">
-  <header>
-    <h5 class="user"><img class="avatar" src="${tweet.user.avatars}">${tweet.user.name}</h5>
-    <p class="handle">${tweet.user.handle}</p>
-    <p class="tweet-message">${tweet.content.text}</p>
-  </header>
-    <footer>
-      <p class="time">${timeago.format(tweet.created_at)}</p>
-      <i class="fa-solid fa-flag"></i>
-      <i class="fa-solid fa-retweet"></i>
-      <i class="fa-solid fa-heart"></i>
-    </footer>
-</article>
-`;
-return $tweetPost;
+  const createTweetElement = function (tweet) {
+    let $tweetPost = `
+    <article class="tweet-border">
+    <header>
+      <h5 class="user"><img class="avatar" src="${tweet.user.avatars}">${
+      tweet.user.name
+    }</h5>
+      <p class="handle">${tweet.user.handle}</p>
+      <p class="tweet-message">${tweet.content.text}</p>
+    </header>
+      <footer>
+        <p class="time">${timeago.format(tweet.created_at)}</p>
+        <i class="fa-solid fa-flag"></i>
+        <i class="fa-solid fa-retweet"></i>
+        <i class="fa-solid fa-heart"></i>
+      </footer>
+  </article>
+  `;
+    return $("#tweet-container").prepend($tweetPost);
   };
 
-  const renderTweets = function(tweetData) {
-    // loops through tweets
-    for(let tweet of tweetData) {
-    // calls createTweetElement for each tweet
-    let fakeTweet = createTweetElement(tweet);
-    $('#tweet-container').append(fakeTweet);
-    }
-    }
+  $("form").submit(function (e) {
+    e.preventDefault();
 
-$(document).ready(function() {
-  renderTweets(data);
-});
+    const formData = $("form").serialize();
 
-$("form").submit(function(e) {
-  e.preventDefault()
-  console.log("Hi there!")
-});
+    $.ajax({
+      url: "/tweets",
+      method: "POST",
+      data: formData,
+    })
+      .then(loadTweets)
+      .catch((err) => console.log(err));
+  });
 
-const formData = $("form").serialize();
-console.log("Hi there!", formData)
-
-$.ajax({
-  url: "/tweets",
-  method: "POST",
-  data: formData,
+  const loadTweets = function () {
+    $.ajax({
+      url: "/tweets",
+      method: "GET",
+    })
+      .then((res) => renderTweets(res))
+      .catch((err) => console.log(err));
+  };
+  loadTweets();
 });
