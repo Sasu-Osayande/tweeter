05ced2dbe56b3c024f3a532336583dfd0c4a7bd7
Sasu Osayande <osayande.sasu@outlook.com>
2022-03-02 16:26:20 -0500

    Implement the rednderTweets function to display the tweets from that data onto the page

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,38 +4,76 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
+// const tweetData = {
+//   "user": {
+//     "name": "Newton",
+//     "avatars": "https://i.imgur.com/73hZDYK.png",
+//       "handle": "@SirIsaac"
+//     },
+//   "content": {
+//       "text": "If I have seen further it is by standing on the shoulders of giants"
+//     },
+//   "created_at": 1461116232227
+// }
+
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
       "handle": "@SirIsaac"
     },
-  "content": {
+    "content": {
       "text": "If I have seen further it is by standing on the shoulders of giants"
     },
-  "created_at": 1461116232227
-}
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
 
-const createTweetElement = function(tweetObject) {
-  return (`
-  <article>
+const createTweetElement = function(tweet) {
+  let $tweetPost = `
+  <article class="tweet-border">
   <header>
-    <h5 class="user"><img class="avatar" src="${tweetObject.user.avatars}">${tweetObject.user.name}</h5>
-    <p class="handle">${tweetObject.user.handle}</p>
-    <p class="tweet-message">${tweetObject.content.text}</p>
+    <h5 class="user"><img class="avatar" src="${tweet.user.avatars}">${tweet.user.name}</h5>
+    <p class="handle">${tweet.user.handle}</p>
+    <p class="tweet-message">${tweet.content.text}</p>
   </header>
     <footer>
-      <p class="time">${timeago.format(tweetObject.created_at)}</p>
+      <p class="time">${timeago.format(tweet.created_at)}</p>
       <i class="fa-solid fa-flag"></i>
       <i class="fa-solid fa-retweet"></i>
       <i class="fa-solid fa-heart"></i>
     </footer>
 </article>
-`);
+`;
+return $tweetPost;
   };
 
-const $tweet = createTweetElement(tweetData);
+   // console.log($tweet); // to see what it looks like
+  // $('#tweet-container').append($tweet);
+  // renderTweets(data);
+  const renderTweets = function(tweetData) {
+    // loops through tweets
+    for(let tweet of tweetData) {
+    // calls createTweetElement for each tweet
+    let fakeTweet = createTweetElement(tweet);
+    $('#tweet-container').append(fakeTweet);
+    }
+    }
+
+// const $tweet = createTweetElement(data);
+
 $(document).ready(function() {
-  console.log($tweet); // to see what it looks like
-  $('#tweets-container').append($tweet);
+  renderTweets(data);
 });
